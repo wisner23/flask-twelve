1c13730ed0c08fb07fdc010cd339684742fe815c
Wisner Oliveira <wisner.oliveira@gmail.com>
2018-02-04 10:59:56 -0200

    Fixing mistake on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 docker build . -t registry.heroku.com/twelve-factors/web
 docker login --username=wisner.oliveira@gmail.com --password=$(echo $HEROKU_TOKEN) registry.heroku.com
-docker push registry.heroku.com./twelve-factors/web
+docker push registry.heroku.com/twelve-factors/web
